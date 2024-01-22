1980e870ba81bc27ad0f7d1073f41cb188b6bff6
Alex Zenla <alex@mycelium.engineering>
2024-01-22 02:26:43 -0800

    hypha: init will now print the command from the image config

--- hypha/src/container/init.rs
+++ hypha/src/container/init.rs
@@ -1,4 +1,5 @@
 use crate::error::Result;
+use oci_spec::image::ImageConfiguration;
 use std::fs;
 use std::path::Path;
 use sys_mount::{FilesystemType, Mount, MountFlags};
@@ -8,6 +9,8 @@ const CONFIG_BLOCK_DEVICE_PATH: &str = "/dev/xvdb";
 
 const IMAGE_MOUNT_PATH: &str = "/image";
 const CONFIG_MOUNT_PATH: &str = "/config";
+
+const IMAGE_CONFIG_JSON_PATH: &str = "/config/image/config.json";
 
 pub struct ContainerInit {}
 
@@ -24,6 +27,12 @@ impl ContainerInit {
 
     pub fn init(&mut self) -> Result<()> {
         self.prepare_mounts()?;
+        let config = self.parse_image_config()?;
+        if let Some(cfg) = config.config() {
+            if let Some(cmd) = cfg.cmd() {
+                println!("image command: {:?}", cmd);
+            }
+        }
         Ok(())
     }
 
@@ -33,6 +42,12 @@ impl ContainerInit {
         self.mount_squashfs(Path::new(IMAGE_BLOCK_DEVICE_PATH), image_mount_path)?;
         self.mount_squashfs(Path::new(CONFIG_BLOCK_DEVICE_PATH), config_mount_path)?;
         Ok(())
+    }
+
+    fn parse_image_config(&mut self) -> Result<ImageConfiguration> {
+        let image_config_path = Path::new(IMAGE_CONFIG_JSON_PATH);
+        let config = ImageConfiguration::from_file(image_config_path)?;
+        Ok(config)
     }
 
     fn mount_squashfs(&mut self, from: &Path, to: &Path) -> Result<()> {
