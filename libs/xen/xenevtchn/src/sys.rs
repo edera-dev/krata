09690c33e29568ac29ea5df7e5d826628d37ca5d
Alex Zenla <alex@mycelium.engineering>
2024-02-15 20:32:41 +0000

    xenevtchn: fix ioctl constants

--- libs/xen/xenevtchn/src/sys.rs
+++ libs/xen/xenevtchn/src/sys.rs
@@ -1,4 +1,4 @@
-use nix::{ioctl_none, ioctl_readwrite_bad, request_code_none};
+use nix::{ioctl_none, ioctl_readwrite_bad};
 use std::ffi::c_uint;
 
 #[repr(C)]
@@ -27,17 +27,9 @@ pub struct Notify {
     pub port: c_uint,
 }
 
-ioctl_readwrite_bad!(bind_virq, request_code_none!(b'E', 0), BindVirq);
-ioctl_readwrite_bad!(
-    bind_interdomain,
-    request_code_none!(b'E', 1),
-    BindInterdomain
-);
-ioctl_readwrite_bad!(
-    bind_unbound_port,
-    request_code_none!(b'E', 2),
-    BindUnboundPort
-);
-ioctl_readwrite_bad!(unbind, request_code_none!(b'E', 3), UnbindPort);
-ioctl_readwrite_bad!(notify, request_code_none!(b'E', 4), Notify);
-ioctl_none!(reset, b'E', 5);
+ioctl_readwrite_bad!(bind_virq, 0x44500, BindVirq);
+ioctl_readwrite_bad!(bind_interdomain, 0x84501, BindInterdomain);
+ioctl_readwrite_bad!(bind_unbound_port, 0x44503, BindUnboundPort);
+ioctl_readwrite_bad!(unbind, 0x44502, UnbindPort);
+ioctl_readwrite_bad!(notify, 0x44504, Notify);
+ioctl_none!(reset, 0x4505, 5);
