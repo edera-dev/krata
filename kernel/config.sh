a5af3ffbe7f1b6937237166b74c8603dd4935160
Alex Zenla <alex@edera.dev>
2024-03-05 21:45:07 +0000

    kernel: bump to 6.7.8

--- kernel/config.sh
+++ kernel/config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-KERNEL_VERSION="6.7.5"
+KERNEL_VERSION="6.7.8"
 KERNEL_SRC_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${KERNEL_VERSION}.tar.xz"
 SRC_DIR_NAME="linux-${KERNEL_VERSION}"
 OUTPUT_DIR_NAME="target"
