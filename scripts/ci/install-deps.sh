dc6505f085dc2e7e0544ca57ef5b88098bf39194
Alex Zenla <alex@edera.dev>
2024-03-07 05:34:15 +0000

    workflows: fix dependencies

--- scripts/ci/install-deps.sh
+++ scripts/ci/install-deps.sh
@@ -2,4 +2,6 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y build-essential libssl-dev libelf-dev flex bison bc protobuf-compiler
+sudo apt-get install -y \
+    build-essential libssl-dev libelf-dev musl-dev \
+    flex bison bc protobuf-compiler musl-tools
