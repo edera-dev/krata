7a9a9abd664e995b6f98f6693a2d453b5f20d832
Alex Zenla <alex@edera.dev>
2024-03-10 12:16:47 +0000

    workflows: install os builder dependencies

--- hack/ci/install-deps.sh
+++ hack/ci/install-deps.sh
@@ -4,5 +4,5 @@ set -e
 sudo apt-get update
 sudo apt-get install -y \
     build-essential libssl-dev libelf-dev musl-dev \
-    flex bison bc protobuf-compiler musl-tools
+    flex bison bc protobuf-compiler musl-tools qemu-utils
 sudo gem install --no-document fpm
