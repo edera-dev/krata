387cce061b4291c6baf883a432048b02c6872c9e
Alex Zenla <alex@edera.dev>
2024-03-21 21:44:37 +0000

    ci: install aarch64 gcc

--- hack/ci/install-deps.sh
+++ hack/ci/install-deps.sh
@@ -4,6 +4,7 @@ set -e
 sudo apt-get update
 sudo apt-get install -y \
     build-essential libssl-dev libelf-dev musl-dev \
-    flex bison bc protobuf-compiler musl-tools qemu-utils
+    flex bison bc protobuf-compiler musl-tools qemu-utils gcc-aarch64-linux-gnu
+
 sudo gem install --no-document fpm
 cargo install cross --git https://github.com/cross-rs/cross
