a8a5412c0e292c7f6659ca0b643e469e40ab47a2
Alex Zenla <alex@edera.dev>
2024-06-20 20:16:07 -0700

    chore(xen): split platform support into separate crate

--- crates/xen/xenplatform/src/error.rs
+++ crates/xen/xenplatform/src/error.rs
@@ -1,13 +1,9 @@
 use std::io;
 
-use crate::pci::PciBdf;
-
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("io issue encountered: {0}")]
     Io(#[from] io::Error),
-    #[error("xenstore issue encountered: {0}")]
-    XenStore(#[from] xenstore::error::Error),
     #[error("xencall issue encountered: {0}")]
     XenCall(#[from] xencall::error::Error),
     #[error("domain does not have a tty")]
@@ -20,6 +16,12 @@ pub enum Error {
     PathParentNotFound,
     #[error("domain does not exist")]
     DomainNonExistent,
+    #[error("elf parse failed: {0}")]
+    ElfParseFailed(#[from] elf::ParseError),
+    #[error("mmap failed")]
+    MmapFailed,
+    #[error("munmap failed: {0}")]
+    UnmapFailed(nix::errno::Errno),
     #[error("memory setup failed: {0}")]
     MemorySetupFailed(&'static str),
     #[error("populate physmap failed: wanted={0}, received={1}, input_extents={2}")]
@@ -36,12 +38,6 @@ pub enum Error {
     GenericError(String),
     #[error("failed to parse int: {0}")]
     ParseIntError(#[from] std::num::ParseIntError),
-    #[error("invalid pci bdf string")]
-    InvalidPciBdfString,
-    #[error("pci device {0} is not assignable")]
-    PciDeviceNotAssignable(PciBdf),
-    #[error("xen platform error: {0}")]
-    XenPlatform(#[from] xenplatform::error::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
