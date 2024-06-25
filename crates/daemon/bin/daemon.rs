88a62441b187f43a4c88e4d1db8c38620cf57308
Khionu Sybiern <khionu@edera.dev>
2024-06-25 12:10:57 -0700

    Initial fluentd support (#205)
    * fix(hack): use sudo -E when running Rust binaries
    
    This makes it possible to pass envvars to the Krata runtime
    
    * feat(o11y): add fluent sink to logs
    
    This change adds fluent logging as an opt-in feature. Setting
    `KRATA_LOG_FLUENT` with an address:port will start a TCP connection,
    sending logs.
    
    A later changes will respect a URI scheme and use structured logging.

--- crates/daemon/bin/daemon.rs
+++ crates/daemon/bin/daemon.rs
@@ -1,15 +1,41 @@
+use std::{
+    net::{SocketAddr, TcpStream},
+    str::FromStr,
+    sync::{atomic::AtomicBool, Arc},
+};
+
 use anyhow::Result;
 use clap::Parser;
-use env_logger::Env;
-use kratad::command::DaemonCommand;
+use env_logger::fmt::Target;
 use log::LevelFilter;
-use std::sync::{atomic::AtomicBool, Arc};
+
+use kratad::command::DaemonCommand;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() -> Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info"))
-        .filter(Some("backhand::filesystem::writer"), LevelFilter::Warn)
-        .init();
+    let mut builder = env_logger::Builder::new();
+    builder
+        .filter_level(LevelFilter::Trace)
+        .parse_default_env()
+        .filter(Some("backhand::filesystem::writer"), LevelFilter::Warn);
+
+    if let Ok(f_addr) = std::env::var("KRATA_FLUENT_ADDR") {
+        println!("KRATA_FLUENT_ADDR set to {f_addr}");
+        let target = SocketAddr::from_str(f_addr.as_str())?;
+        builder.target(Target::Pipe(Box::new(TcpStream::connect(target)?)));
+    }
+
+    let ev = std::env::vars()
+        .into_iter()
+        .fold(String::new(), |mut acc, (k, v)| {
+            acc.push_str(&format!("{k}={v}\n"));
+            acc
+        });
+
+    std::fs::write("/var/log/krata/ev", ev)?;
+
+    builder.init();
+
     mask_sighup()?;
 
     let command = DaemonCommand::parse();
