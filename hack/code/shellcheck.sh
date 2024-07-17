28d63d7d7037725eacd9b988efc34477e4a282c7
Alex Zenla <alex@edera.dev>
2024-07-16 19:02:47 -0700

    chore(cleanup): remove legacy OS technology demo (#256)

--- hack/code/shellcheck.sh
+++ hack/code/shellcheck.sh
@@ -5,4 +5,3 @@ REAL_SCRIPT="$(realpath "${0}")"
 cd "$(dirname "${REAL_SCRIPT}")/../.."
 
 find hack -type f -name '*.sh' -print0 | xargs -0 shellcheck -x
-find os/internal -type f -name '*.sh' -print0 | xargs -0 shellcheck -x
