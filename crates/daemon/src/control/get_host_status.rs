d7a744dde44908af030a5ff51b38d55c50fb81a1
Alex Zenla <alex@edera.dev>
2024-08-24 22:57:57 -0700

    fix(rpc): rename HostStatus to GetHostStatus

--- crates/daemon/src/control/get_host_status.rs
+++ crates/daemon/src/control/get_host_status.rs
@@ -2,21 +2,21 @@ use crate::command::DaemonCommand;
 use crate::ip::assignment::IpAssignment;
 use crate::zlt::ZoneLookupTable;
 use anyhow::Result;
-use krata::v1::control::{HostStatusReply, HostStatusRequest};
+use krata::v1::control::{GetHostStatusReply, GetHostStatusRequest};
 
-pub struct HostStatusRpc {
+pub struct GetHostStatusRpc {
     ip: IpAssignment,
     zlt: ZoneLookupTable,
 }
 
-impl HostStatusRpc {
+impl GetHostStatusRpc {
     pub fn new(ip: IpAssignment, zlt: ZoneLookupTable) -> Self {
         Self { ip, zlt }
     }
 
-    pub async fn process(self, _request: HostStatusRequest) -> Result<HostStatusReply> {
+    pub async fn process(self, _request: GetHostStatusRequest) -> Result<GetHostStatusReply> {
         let host_reservation = self.ip.retrieve(self.zlt.host_uuid()).await?;
-        Ok(HostStatusReply {
+        Ok(GetHostStatusReply {
             host_domid: self.zlt.host_domid(),
             host_uuid: self.zlt.host_uuid().to_string(),
             krata_version: DaemonCommand::version(),
